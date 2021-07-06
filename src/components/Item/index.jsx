ba23b8de19a1d4c82cd5564fe8d1c8e87d4452be
NeosWang <neos_wang@yahoo.com>
2021-07-06 09:03:28 +0200

    c/d tasks

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -3,26 +3,36 @@ import './index.css'
 
 export default class Footer extends Component {
 
-state={mouse:false}
+    state = { mouse: false }
 
-    handleMouse=(flag) => {
-        return ()=>{
-            this.setState({mouse:flag})
+    handleMouse = (flag) => {
+        return () => {
+            this.setState({ mouse: flag })
         }
     }
 
+    handleChange = (id) => {
+        return (event) => {
+            this.props.updateTodo(id, event.target.checked)
+        }
+    }
+
+    handleDelete = (id) => {
+        if(window.confirm('confirm to remove'))        this.props.deleteTodo(id)
+    }
+
     render() {
-        const {name, done} = this.props
-        const {mouse} = this.state
+        const { id, name, done } = this.props
+        const { mouse } = this.state
         return (
-            <li style={{backgroundColor:mouse? '#ddd':'white'}} 
-            onMouseLeave={this.handleMouse(false)} 
-            onMouseEnter={this.handleMouse(true)}>
+            <li style={{ backgroundColor: mouse ? '#ddd' : 'white' }}
+                onMouseLeave={this.handleMouse(false)}
+                onMouseEnter={this.handleMouse(true)}>
                 <label>
-                    <input type="checkbox" defaultChecked={done} />
+                    <input type="checkbox" defaultChecked={done} onChange={this.handleChange(id)} />
                     <span>{name}</span>
                 </label>
-                <button className="btn btn-danger" style={{ display: mouse?'block':'none' }}>remove</button>
+                <button className="btn btn-danger" style={{ display: mouse ? 'block' : 'none' }} onClick={() => this.handleDelete(id)}>Remove</button>
             </li>
         )
     }
