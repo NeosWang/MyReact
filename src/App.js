612ab4853131bf46ff8c33b87046bc2f17250d69
NeosWang <neos_wang@yahoo.com>
2021-07-10 23:28:38 +0200

    router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Header from './components/Header'
+import MyNavLink from './components/MyNavLink'
 import './App.css'
-import Search from './components/Search'
-import List from './components/List'
-// import axios from 'axios'
 
 export default class App extends Component {
-
-
-
   render() {
     return (
       <div>
-        <div className="container">
-          <Search />
-          <List />
+        <div className="row">
+          <div className="col-xs-offset-2 col-xs-8">
+            <Header />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2 col-xs-offset-2">
+            <div className="list-group">
+              {/* <a className="list-group-item" href="./about.html">About</a>
+              <a className="list-group-item active" href="./home.html">Home</a> */}
+              <MyNavLink to='/home'>Home</MyNavLink>
+              <MyNavLink to='/about'>About</MyNavLink>
+            </div>
+          </div>
+          <div className="col-xs-6">
+            <div className="panel">
+              <div className="panel-body">
+                <Switch>
+                  <Route path="/about" component={About} />
+                  <Route path="/home" component={Home} />
+                </Switch>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     )
